a8bcb9b5251441f08ba53558eab44494824e3b24
Subin002 <subindask002@gmail.com>
2025-11-07 11:56:55 +0530

    Fixed start file for Render3

--- config/db.js
+++ config/db.js
@@ -2,14 +2,18 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/carDB", {
+   
+    const mongoUri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/carDB";
+
+    await mongoose.connect(mongoUri, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("✅ MongoDB Connected");
+
+    console.log(`✅ MongoDB Connected: ${mongoose.connection.host}`);
   } catch (error) {
     console.error("❌ MongoDB Connection Failed:", error.message);
-    process.exit(1);
+    process.exit(1); 
   }
 };
 
--- index.js
+++ index.js
@@ -13,5 +13,5 @@ connectDB();
 
 app.use("/api/cars", carRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
